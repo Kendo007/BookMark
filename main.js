3985c01862a361c5b850af4be71f2cdd540794da
Kendo007 <110466301+Kendo007@users.noreply.github.com>
2024-09-18 22:10:51 +0530

    Added add book form and book class

---
+++ main.js
@@ -0,0 +1,8 @@
+class Book {
+    constructor(title, author, pagesRead, totalPages) {
+        this.title = title;
+        this.author = author;
+        this.pagesRead = pagesRead;
+        this.totalPages = totalPages;
+    }
+}
