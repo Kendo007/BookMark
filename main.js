d5c300186746ed57ac2e847f63650151e2918e2a
Kendo007 <110466301+Kendo007@users.noreply.github.com>
2024-09-19 19:44:21 +0530

    Added visibility and disapperance

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+function toggle() {
+    var blur = document.getElementById("body");
+    blur.classList.toggle("active");
+
+    var popup = document.getElementById("menu");
+    popup.classList.toggle("active");
+}
+
 class Book {
     constructor(title, author, pagesRead, totalPages) {
         this.title = title;
