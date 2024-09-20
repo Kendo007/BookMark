3b5014d9104fae94b1bea61ea9ac4b272f6983f5
Kendo007 <110466301+Kendo007@users.noreply.github.com>
2024-09-20 12:21:20 +0530

    Added Book and Storage class in JS

--- main.js
+++ main.js
@@ -14,3 +14,45 @@ class Book {
         this.totalPages = totalPages;
     }
 }
+
+class Storage {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem("books") === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem("books"));
+        }
+        return books;
+    }
+
+    static addBook(book) {
+        const books = Storage.getBooks();
+        books.push(book);
+        localStorage.setItem("books", JSON.stringify(books));
+    }
+    
+    static removeBook(bookTitle) {
+        const books = Storage.getBooks();
+
+        books.forEach((book, index) => {
+            if (book.title === bookTitle) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem("books", JSON.stringify(books));
+    }
+
+    static updateBook(bookTitle, pagesRead) {
+        const books = Storage.getBooks();
+
+        books.forEach((book) => {
+            if (book.title === bookTitle) {
+                book.pagesRead = pagesRead;
+            }
+        });
+
+        localStorage.setItem("books", JSON.stringify(books));
+    }
+}
